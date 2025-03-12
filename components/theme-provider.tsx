bf9063fc6560ca3eacb435a290f7817e698bee78
austin207 <austinantony06@gmail.com>
2025-03-12 18:43:39 +0530

    Add untracked changes in virtue-v1

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,22 +1,8 @@
-// components/theme-provider.tsx
-"use client";
+"use client"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import type { ThemeProviderProps } from "next-themes"
 
-import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-
-interface ThemeProviderProps {
-  children: React.ReactNode;
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
 
-export function ThemeProvider({ children }: ThemeProviderProps) {
-  return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="dark"
-      enableSystem={false}
-      disableTransitionOnChange
-    >
-      {children}
-    </NextThemesProvider>
-  );
-}
